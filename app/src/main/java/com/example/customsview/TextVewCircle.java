97c3156c5c21e2dc06f9194f6e58f46a3d302979
Igophuong <anhphuong8951@gmail.com>
2020-08-24 19:29:25 +0700

    Initial commit

---
+++ app/src/main/java/com/example/customsview/TextVewCircle.java
@@ -0,0 +1,21 @@
+package com.example.customsview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+public class TextVewCircle extends LinearLayout {
+    public TextVewCircle(Context context) {
+        super(context);
+    }
+
+    public TextVewCircle(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public TextVewCircle(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
